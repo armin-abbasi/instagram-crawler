60485d5931cba4351efd42257f579e47db727e8f
Armin Abbasi <armin.abbasi@hotmail.com>
2019-07-12 16:40:52 +0430

    added mongodb configuration variables

--- config/database.php
+++ config/database.php
@@ -82,6 +82,18 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_DB_HOST', 'localhost'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE'),
+            'username' => env('MONGO_DB_USERNAME'),
+            'password' => env('MONGO_DB_PASSWORD'),
+            'options' => [
+                'database' => 'admin' // sets the authentication database required by mongo 3
+            ]
+        ],
+
     ],
 
     /*
