4ca07c07989c5734ab6cf60b81ffb558c774ff97
Armin Abbasi <armin.abbasi@hotmail.com>
2019-07-12 15:46:44 +0430

    installed moloquent package

---
+++ app/Http/Controllers/CrawlerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class CrawlerController extends Controller
+{
+    public function getPosts()
+    {
+        
+    }
+}
