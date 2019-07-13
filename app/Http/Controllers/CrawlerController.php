14941974d4e6d92a2fbb3cea647d3d0a11ae67ff
armin <armin.abbasi@hotmail.com>
2019-07-13 15:28:20 +0430

    created getNodes prototype method

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -11,21 +11,33 @@ class CrawlerController extends Controller
     public function getPosts()
     {
         $uri = 'https://www.instagram.com/explore/tags/%D9%81%D8%B1%D8%A7%D8%AA%D8%B1%D8%A8%D8%B1%D9%88/?__a=1';
-        $post = new Post();
-        $client = new Client();
 
         try {
-            $response = json_decode($client->request('GET', $uri)->getBody()->getContents());
-
-            $edges = $response->graphql->hashtag->edge_hashtag_to_media->edges;
-
-            foreach ($edges as $edge) {
-                $post->insert((array)$edge);
-            }
-
-            return $post->count();
+            $this->getNodes($uri);
+            return Post::all()->count();
         } catch (\Exception $e) {
             \Log::error($e->getMessage());
+        }
+    }
+
+    private function getNodes($uri, $maxId = null)
+    {
+        $client = new Client();
+        $post = new Post();
+
+        // Add Max ID if included.
+        $uri = $maxId ? $uri . '&max_id=' . $maxId : $uri;
+
+        $response = json_decode($client->request('GET', $uri)->getBody()->getContents());
+
+        $edge_info = $response->graphql->hashtag->edge_hashtag_to_media;
+
+        foreach ($edge_info->edges as $edge) {
+            $post->insert((array)$edge);
+        }
+
+        if ($edge_info->page_info->has_next_page) {
+            $this->getNodes($uri, $edge_info->page_info->end_cursor);
         }
     }
 }
