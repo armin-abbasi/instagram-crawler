74339d349cfef2ea5fbaf5748da4198f695063fe
armin <armin.abbasi@hotmail.com>
2019-07-13 12:57:54 +0430

    insert data into model

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
@@ -9,6 +10,22 @@ class CrawlerController extends Controller
 {
     public function getPosts()
     {
-        
+        $uri = 'https://www.instagram.com/explore/tags/%D9%81%D8%B1%D8%A7%D8%AA%D8%B1%D8%A8%D8%B1%D9%88/?__a=1';
+        $post = new Post();
+        $client = new Client();
+
+        try {
+            $response = json_decode($client->request('GET', $uri)->getBody()->getContents());
+
+            $edges = $response->graphql->hashtag->edge_hashtag_to_media->edges;
+
+            foreach ($edges as $edge) {
+                $post->insert((array)$edge);
+            }
+
+            return $post->count();
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+        }
     }
 }
